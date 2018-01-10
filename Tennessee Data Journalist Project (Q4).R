db4d96eca7f258d6e3f75d07387811b9301c47c2
James CarMichael <carmijh0@gmail.com>
2018-01-10 14:57:07 -0600

    merged core and zip

--- Tennessee Data Journalist Project (Q4).R
+++ Tennessee Data Journalist Project (Q4).R
@@ -4,7 +4,7 @@ library("readxl")
 #Importing the zip code and core dataframes.  
 
 core <- read_csv("data/achievement_profile_data_with_CORE.csv")
-zips <- zip_code_database
+zips <- read_excel("data/zip_code_database.xlsx")
 View(core)
 View(zips)
 
@@ -18,17 +18,21 @@ TN_zips <- zips %>%
 TN_zips <- TN_zips[-c(2, 3, 5, 6, 9:12)]
 View(TN_zips)
 
-#Removing system
+#Removing the system column, and adding aggregate columns for each discipline.
 
 core <- core[-c(1)]
-View(core)
-
-#Adding aggregate columns for each discipline
-
+colnames(core)[1] <- 'county'
 core <- core %>%
   rowwise() %>%
   mutate(avg_Math = mean(c(Math, AlgI, AlgII), na.rm = TRUE),
          avg_Eng = mean(c(ELA, EngI, EngII, EngIII), na.rm = TRUE),
          avg_Sci = mean(c(Science, BioI, Chemistry), na.rm = TRUE))
+
+#Attempting to merge....
+
+merged_county <- core %>% 
+  inner_join(TN_zips, by = 'county')
+
+View(merged_county)
 
 #------------------------------------------------------------------------------------
